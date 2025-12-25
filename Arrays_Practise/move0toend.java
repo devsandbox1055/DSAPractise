b910fd765b7e4edfe8dfe6f01dc5d773ae497817
devsandbox1055 <deveshojha350@gmail.com>
2025-12-25 21:40:46 +0530

    today practise

---
+++ Arrays_Practise/move0toend.java
@@ -0,0 +1,22 @@
+git add arrays/public class move0toend {
+    public static void main(String[] args) {
+        int[] arr  = {0,1,0,0,0,1};
+        int  j = 0;
+
+        for(int i = 0; i<arr.length; i++) {
+            if(arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        for(int i = 0; i<arr.length; i++) {
+            System.out.println( arr[i] +" ");
+        }
+
+
+    }
+}
+
+ 
