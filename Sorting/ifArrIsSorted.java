63043326d0cde35892fc4e4f96e83b8b7e54b729
devsandbox1055 <deveshojha350@gmail.com>
2025-12-27 16:03:09 +0530

    question done

--- Sorting/ifArrIsSorted.java
+++ Sorting/ifArrIsSorted.java
@@ -1,5 +1,21 @@
 public class ifArrIsSorted {
     public static void main(String[] args) {
+        int[] arr = {1,4,2,1};
+        boolean isSorted  = true;
         
+        for(int i = 0; i<arr.length; i++) {
+            if(arr[i] < arr[i-1]) {
+                isSorted = false;
+                break;
+
+            }
+            
+        }
+        if (isSorted) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+
     }
 }
