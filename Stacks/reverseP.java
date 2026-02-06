1154de9cdceed13b94648150a00c7bf335a01883
devsandbox1055 <deveshojha350@gmail.com>
2026-02-06 21:10:40 +0530

    practise

---
+++ Stacks/reverseP.java
@@ -0,0 +1,19 @@
+﻿public class reverseP {
+    // Reverses a stack using recursion
+    public static void reverse(java.util.Stack<Integer> stack) {
+        if (stack.isEmpty()) return;
+        int top = stack.pop();
+        reverse(stack);
+        insertAtBottom(stack, top);
+    }
+
+    private static void insertAtBottom(java.util.Stack<Integer> stack, int value) {
+        if (stack.isEmpty()) {
+            stack.push(value);
+            return;
+        }
+        int top = stack.pop();
+        insertAtBottom(stack, value);
+        stack.push(top);
+    }
+}
